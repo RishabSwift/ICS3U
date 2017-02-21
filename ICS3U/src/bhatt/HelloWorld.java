dbcffa482408263ea3a7e79bdd061d425ab0b085
Rishab Bhatt <rishabswift@gmail.com>
2017-02-21 11:01:57 -0500

    Added comments

--- ICS3U/src/bhatt/HelloWorld.java
+++ ICS3U/src/bhatt/HelloWorld.java
@@ -1,5 +1,13 @@
 package bhatt;
 
+/**
+ * 
+ * HelloWorld.java
+ * This program prints "Hello, World!" to the console.
+ * Feb 21, 2017
+ * @author Rishab Bhatt
+ *
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
