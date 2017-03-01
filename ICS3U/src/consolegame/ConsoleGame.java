be8c883e1ecf47042b820a9481f0adb398ac8e33
Rishab Bhatt <rishabswift@gmail.com>
2017-03-01 10:39:59 -0500

    Console

--- ICS3U/src/consolegame/ConsoleGame.java
+++ ICS3U/src/consolegame/ConsoleGame.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-
+import hsa_new.Console;;
 
 public class ConsoleGame {
 
 	private boolean isDead;
 	private String items[] = new String[] { "keys", "backpack", "badge", "gun" };
 	private boolean hasGuy;
+
 	private Scanner scan = new Scanner(System.in);
 
 	enum Location {
@@ -21,101 +22,106 @@ public class ConsoleGame {
 	private ArrayList<Location> unlockedPlaces;
 	private ArrayList<Location> locationHistory;
 	private Location unlockedLocations[] = new Location[Location.values().length];
+
 	// The current user input is whatever the user entered last in console
 	private String userInput;
 
 	public static void main(String[] args) {
-		
-		 ConsoleGame consoleGame =new ConsoleGame();
-	        consoleGame.resetGame();
-	        consoleGame.playGame();
+		ConsoleGame consoleGame = new ConsoleGame();
+		consoleGame.resetGame();
+		consoleGame.playGame();
 	}
+
 	
 	private void playGame() {
-        askQuestion("Are you gay?", "yes:yeah");
-        if (userInput.equals("yes")) {
-            showMessage("No problem. Everyone is...");
-        } else {
-            showMessage("Ah. ok.");
-        }
-
-        askQuestion("Where do you wanna go?", "china:africa:fucking");
-        if (userInput.equals("africa")) {
-            showMessage("Fuck, every one loves africa!");
-        } else {
-            showMessage("Shit! I love china. fucking chinese condoms is teh reason i have a baby");
-        }
-    }
-
-    /**
-     * Show user a message. Instead of
-     * @param message
-     */
-    private void showMessage(String message) {
-        System.out.println(message);
-    }
+		
+	
+		askQuestion("Are you gay?", "yes:yeah");
 
 
-    /**
-     * Reset the game and set all variables to their default
-     */
-    private void resetGame() {
-        this.currentLocation = Location.OPERATING_ROOM;
-        this.unlockedLocations = null;
-        this.locationHistory = null;
-        this.hasGuy = false;
-    }
+		// askQuestion("Where do you wanna go?", "china:africa:fucking");
+		// if (userInput.equals("africa")) {
+		// showMessage("Fuck, every one loves africa!");
+		// } else {
+		// showMessage("Shit! I love china. fucking chinese condoms is teh
+		// reason i have a baby");
+		// }
+	}
 
-    private void setUnlockedItems(String itemName) {
+	/**
+	 * Show user a message. Instead of
+	 * 
+	 * @param message
+	 */
+	private void showMessage(String message) {
+		System.out.println(message);
+	}
 
-    }
+	/**
+	 * Reset the game and set all variables to their default
+	 */
+	private void resetGame() {
+		this.currentLocation = Location.OPERATING_ROOM;
+		this.unlockedLocations = null;
+		this.locationHistory = null;
+		this.hasGuy = false;
+	}
 
-    /**
-     * Used to ask question to user.
-     * Keep asking until user has entered an acceptable answer
-     *
-     * @param question        the question to ask user
-     * @param acceptedAnswers list of accepted answers separated by colon ":"
-     * @return user's input
-     */
-    private String askQuestion(String question, String acceptedAnswers) {
-        String input;
-        while (true) {
-            System.out.println(question + " ( " + acceptedAnswers.replaceAll(":", " / ") + " )");
-            // Convert user input to lower case so it's easier to parse
-            input = scan.nextLine().toLowerCase();
-            // Validate user's input
-            String message = validateInput(input, acceptedAnswers);
-            if (message.equals("")) {
-                break;
-            }
-            System.out.println(message);
-        }
-        // Set the current user input to the input
-        this.userInput = input;
-        return input;
-    }
+	private void setUnlockedItems(String itemName) {
 
-    /**
-     * Validate the user input with the accepted input.
-     * @param userInput
-     * @param acceptedInput
-     * @return
-     */
-    private String validateInput(String userInput, String acceptedInput) {
+	}
 
-        String message = "";
+	/**
+	 * Used to ask question to user. Keep asking until user has entered an
+	 * acceptable answer
+	 *
+	 * @param question
+	 *            the question to ask user
+	 * @param acceptedAnswers
+	 *            list of accepted answers separated by colon ":"
+	 * @return user's input
+	 */
+	private String askQuestion(String question, String acceptedAnswers) {
+		String input;
+		while (true) {
+			System.out.println(question + " ( " + acceptedAnswers.replaceAll(":", " / ") + " )");
+			// Convert user input to lower case so it's easier to parse
+			input = scan.nextLine().toLowerCase();
+			// Validate user's input
+			String message = validateInput(input, acceptedAnswers);
+			if (message.equals("")) {
+				break;
+			}
+			System.out.println(message);
+		}
+		// Set the current user input to the input
+		this.userInput = input;
+		return input;
+	}
 
-        // split the accept answer into an acceptable array
-        String[] acceptedInputArray = acceptedInput.split(":");
+	/**
+	 * Validate the user input with the accepted input.
+	 * 
+	 * @param userInput
+	 * @param acceptedInput
+	 * @return
+	 */
+	private String validateInput(String userInput, String acceptedInput) {
 
-        // Check if the user's input equals to the accepted input by converting it to a List first
-        List<String> acceptedList = Arrays.asList(acceptedInputArray);
-         if (!acceptedList.contains(userInput)) {
-             message = "Opps! You must either type \"" + acceptedInput.replace(":", "\" or \"") + "\". Please try again.";
-        }
+		String message = "";
 
-        return message;
-    }
+		// split the accept answer into an acceptable array
+		String[] acceptedInputArray = acceptedInput.split(":");
+
+		// Check if the user's input equals to the accepted input by converting
+		// it to a List first
+		List<String> acceptedList = Arrays.asList(acceptedInputArray);
+		if (!acceptedList.contains(userInput)) {
+			message = "Opps! You must either type \"" + acceptedInput.replace(":", "\" or \"")
+					+ "\". Please try again.";
+		}
+
+		return message;
+	}
 
 }
