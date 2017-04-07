9d4af147627d5e87f43a863bf1800a01cfbdf80e
Rishab Bhatt <rishabswift@gmail.com>
2017-04-07 11:13:18 -0400

    Fixed reverse words

--- ICS3U/src/bhatt/unit1/ReverseWords.java
+++ ICS3U/src/bhatt/unit1/ReverseWords.java
@@ -14,13 +14,49 @@ public class ReverseWords {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.print("Please enter a sentence: ");
+
         String sentence = scan.nextLine();
+        String words[] = sentence.split(" ");
 
-        for (int i = sentence.length() - 1; i >= 1; i--) {
-            System.out.print(sentence.charAt(i));
+        // Loop through each word
+        for (String word : words) {
+
+            String endOfWord = "";
+
+            // If you don't want to use regex.. this is simpler
+//            word.contains(".") || word.contains("!") || word.contains("?")
+            if (word.matches("[^.!?\\s][^.!?]*(?:[.!?](?!['\"]?\\s|$)[^.!?]*)*[.!?]?['\"]?(?=\\s|$)")) {
+                String punctuation = Character.toString(word.charAt(word.length() - 1));
+                word = word.replace(punctuation, "");
+                endOfWord += punctuation;
+            }
+
+            // If the word has a upper case or something...
+            if (Character.isUpperCase(word.charAt(0))) {
+                // Set the first letter to lower case
+                char firstLetter = Character.toLowerCase(word.charAt(0));
+
+                // Set the last letter to upper case
+                char lastLetter = Character.toUpperCase((word.charAt(word.length() - 1)));
+                word = word.substring(1, word.length() - 1);
+                word = firstLetter + word + lastLetter;
+
+            }
+
+            // Loop through each letter in the word
+            for (int j = word.length() - 1; j >= 0; j--) {
+                System.out.print(word.charAt(j));
+            }
+
+            // Put any punctuations at the end of the word
+            System.out.print(endOfWord);
+
+            // Don't add the trailing space after the last word
+            System.out.print(" ");
+
         }
-
 
     }
 }
