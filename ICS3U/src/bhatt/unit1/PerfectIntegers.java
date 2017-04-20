79e3240b6c02745350eab8e5a9f67a42c8850d2f
Rishab Bhatt <rishabswift@gmail.com>
2017-04-20 11:35:26 -0400

    Cleaned up code

--- ICS3U/src/bhatt/unit1/PerfectIntegers.java
+++ ICS3U/src/bhatt/unit1/PerfectIntegers.java
@@ -10,31 +10,22 @@ package bhatt.unit1;
 
 public class PerfectIntegers {
 
-
     public static void main(String[] args) {
 
         int lastNumber = 5000;
 
         // Loop up to the last number
         for (int i = 2; i < lastNumber; i++) {
-
             int sum = 0;
-//            System.out.print("Factor of " + i + ": ");
-
             for (int j = 1; j < i; j++) {
                 // Check if number (i) is divisible by j, if so, it's one of the factors
                 if (i % j == 0) {
-//                    System.out.print(j + ", ");
                     sum += j;
                 }
             }
-
             if (sum == i) {
                 System.out.println(i);
             }
-
-//            System.out.println();
-//            System.out.println("Total Sum was: " + sum);
         }
     }
 }
